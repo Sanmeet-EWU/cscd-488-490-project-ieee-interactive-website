079857739beebf5e6b4e42389b50df1427d2b0c5
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2024-11-11 19:17:43 -0800

    Added a test

--- website/src/setupTests.js
+++ website/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Add a unit test
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
