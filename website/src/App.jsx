19283bad7d833247ada3e2d698fff32db21f3348
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2024-12-16 19:39:30 -0800

    Added Logo component and styles for IEEE logo; updated LHSNav style

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import LHSNav from "./Components/LHSNav";
 import Footer from "./Components/Footer";
 import TopNavBar from "./Components/TopNavBar";
+import Logo from "./Components/logo";
 
 
 const App = () => {
   return (
     <div>
+      <Logo />
       <TopNavBar />
       <LHSNav />
       <Footer />
