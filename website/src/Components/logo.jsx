8aac065cb690d195832c51cd7410d6d90012c011
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2024-12-16 19:40:38 -0800

    Updated Logo Component

--- website/src/Components/logo.jsx
+++ website/src/Components/logo.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import './Logo.css';
 import ieeeLogo from '../Assets/IEEE.png';
-import Kurukuru from '../Assets/Kurukuru.gif';
+//import Kurukuru from '../Assets/Kurukuru.gif';
 
 const Logo = () => {
   return (
-    /*<header className = "logo-header">
-      <img src = {ieeeLogo} alt = "IEEE Logo" className = "ieee-logo"/>
-    </header>*/
-    
-    // Mess around
     <header className = "logo-header">
-      <img src = {ieeeLogo} alt = "Kurukuru Logo" className = "kurukuru-logo"/>
+      <img src = {ieeeLogo} alt = "IEEE Logo" className = "ieee-logo"/>
     </header>
   );
 };
