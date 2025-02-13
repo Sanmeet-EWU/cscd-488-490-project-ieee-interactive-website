3543ab723e466837cc65081d9d8580f60c8418ca
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2025-02-13 03:09:54 -0800

    Update navigation styles and modify feature titles on Home page

--- Frontend/src/Components/logo.jsx
+++ Frontend/src/Components/logo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import {Link}from "react-router-dom";
 import './Logo.css';
 import ieeeLogo from '../Assets/IEEE.png';
 
 const Logo = () => {
   return (
     <header className = "logo-header">
+      <Link to="/" className = "logo-link">
       <img src = {ieeeLogo} alt = "IEEE Logo" className = "ieee-logo"/>
+      </Link>
     </header>
   );
 };
