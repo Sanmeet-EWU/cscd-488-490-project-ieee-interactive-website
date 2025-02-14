b02cefb37979ddd7690d7156896eaa3e66c9645e
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2025-02-14 04:50:45 -0800

    Add Photo Gallery page with routing and basic layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,13 +9,14 @@ import LHSNav from "./Components/LHSNav";
 import Home from "./Pages/Home/Home";
 import Officers from "./Pages/Officers/Officers";
 import FormerOfficers from "./Pages/FormerOfficers/FormerOfficers";
+import PhotoGallery from "./Pages/PhotoGallery/PhotoGallery";
 import ContactForm from "./Pages/ContactForm/ContactForm";
 import Admin from "./Pages/Admin/Admin";
 import AdminDashboard from "./Pages/Admin/AdminDashboard";
 import Events from "./Pages/Events/Events";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import Footer from "./Components/Footer";
-import Logo from "./Components/logo";
+import Logo from "./Components/Logo";
 import TopNavBar from "./Components/TopNavBar";
 import "./styles/theme.css";
 
@@ -43,6 +44,7 @@ const App = () => {
               <Route path="/events" element={<Events />} />
               <Route path="/officers" element={<Officers />} />
               <Route path="/former-officers" element={<FormerOfficers />} />
+              <Route path="/photo-gallery" element={ <PhotoGallery />} />
               <Route path="/contact" element={<ContactForm />} />
               <Route path="/admin" element={<Admin />} />
               <Route
